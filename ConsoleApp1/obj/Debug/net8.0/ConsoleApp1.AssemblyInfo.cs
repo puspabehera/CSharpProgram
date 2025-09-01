db414881a2fa7793df761c5929c5af9fd8f1c1a8
puspa behera <puspa.dbdev@gmail.com>
2025-09-02 00:31:41 +0530

    Remove tracked build files using updated .gitignore

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8805807b8de81d6b62762544a3db7d42006862ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6530e2a2ddf561f02a3deb2d64ccaa3aa66f733d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
